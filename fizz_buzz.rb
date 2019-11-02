0f8aae7e763e3774af0e754777d56878253e4d82
長谷川雄大 <hasegawayuudai@hasegawayuudainoMacBook-Air.local>
2019-11-02 18:12:06 +0900

    [Finish]2-9

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,5 +1,21 @@
-number = gets.to_i
+# fizz_buzz.rb
 
-a = number % 3 == 0
-
-puts "#{a}"
+def fizz_buzz(number)
+   if number % 3 == 0 && number % 5 == 0
+   	puts "FizzBuzz"
+   elsif number % 5 == 0
+    puts "buzz"
+   elsif number % 3 == 0
+   	 puts "fizz"
+   else
+   	puts "#{number}"
+   end
+end
+ 
+puts "1 以上の数字を入力してください"
+ 
+input = gets.to_i
+ 
+puts '結果は、、、'
+ 
+puts fizz_buzz(input)
